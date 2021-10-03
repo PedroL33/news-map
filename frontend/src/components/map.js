3e0988b9d0e5911fbb932bcc34cbd034405600a2
Pedrol33 <leepeter019@gmail.com>
2021-10-02 23:18:00 -0700

    Maps api added.

---
+++ frontend/src/components/map.js
@@ -0,0 +1,21 @@
+import React, { useRef, useEffect } from 'react';
+
+const Map = () => {
+
+    const ref = useRef();
+
+    useEffect(() => {
+        const zoom = 3;
+        new window.google.maps.Map(ref.current, {
+            center: {lat: 41.850033, lng:-87.6500523},
+            zoom,
+        });
+    });
+
+    return (
+        <div id="map" ref={ref} style={{ width: '100vw', height: '100vh' }}>
+        </div>
+    )
+}
+
+export default Map;
