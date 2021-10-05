21e13387b61f2b08475bdb8aa7c6d06d5f653b3c
Pedrol33 <leepeter019@gmail.com>
2021-10-04 22:51:56 -0700

    Added loader and error message for map wrapper render function.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Map from './components/map';
-import { Wrapper } from "@googlemaps/react-wrapper";
+import Spinner from './components/spinner';
+import Error from './components/error';
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import React, { ReactElement } from 'react';
+
+const render = (status) => {
+  if (status === Status.LOADING) return <Spinner />;
+  if (status === Status.FAILURE) return <Error />;
+  return null;
+};
 
 function App() {
+  
   return (
     <div className="App">
-      <Wrapper apiKey={process.env.GOOGLEMAPS_API_KEY}>
+      <Wrapper apiKey={process.env.GOOGLEMAPS_API_KEY} render={ render }>
         <Map />
       </Wrapper>
     </div>
---
+++ frontend/src/components/error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    
+    return (
+        <div>
+            Something went wrong.
+        </div>
+    )
+}
+
+export default Error;
