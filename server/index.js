c721184736067f5c2d3e18a1a45834162dfb2b94
Pedrol33 <leepeter019@gmail.com>
2021-10-09 22:03:36 -0700

    RSS news parsing route added.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
+const Parser = require('rss-parser')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/news', async (req, res) => {
+  const parser = new Parser();
+  const feed = await parser.parseURL('https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml')
+
+  let items = [];
+  await Promise.all(feed.items.map(async (currentItem) => {
+    items.push(currentItem);
+  }))
+
+  res.json(items)
 })
 
 app.listen(port, () => {
